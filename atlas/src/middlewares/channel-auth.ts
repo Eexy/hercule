7907b8f134b4c67030d27ef7534355f85caedf4e
unknown <eexdhe@gmail.com>
2021-05-17 10:56:38 +0200

    create route to post message et get messages' list

--- atlas/src/middlewares/channel-auth.ts
+++ atlas/src/middlewares/channel-auth.ts
@@ -2,7 +2,12 @@ import { RequestHandler } from 'express';
 import Channel from '../models/channel';
 
 const channelAuth: RequestHandler = async (req, res, next): Promise<any> => {
-  const channel = await Channel.findById(req.params.id);
+  let channel = null;
+  try{
+    channel = await Channel.findById(req.params.id);
+  }catch(e){
+    return res.send({ok: false, err: 'Invalid channel\'s ID'});
+  }
 
   if (!channel) {
     return res.send({ ok: false, err: 'Unable to find channel, Invalid ID' });
--- atlas/src/routers/channel.ts
+++ atlas/src/routers/channel.ts
@@ -1,34 +1,41 @@
 import express, { Router } from 'express';
-import Message from '../models/message';
-import Channel from '../models/channel';
 import auth from '../middlewares/auth';
+import channelAuth from '../middlewares/channel-auth';
+import Message from '../models/message';
 
 const router: Router = express.Router();
 
-router.get('/api/channel/:id', auth, async (req, res) => {
-  let channel = null;
+router.get('/api/channel/:id', auth, channelAuth, async (req, res) =>
+  res.send({ ok: true, channel: req.channel })
+);
+
+router.get('/api/channel/:id/messages', auth, channelAuth, async (req, res) => {
+  const { channel } = req;
 
   try {
-    channel = await Channel.findById(req.params.id);
-    if (!channel) {
-      throw new Error('Unable to find channel');
-    }
-
-    const isRecipient = channel.recipients.find(
-      (recipient) => recipient.toString() === req.user.id
-    );
-
-    if (!isRecipient) {
-      throw new Error('You are not allowed to access this channel');
-    }
-
-    await channel.populate({path: 'messages'}).execPopulate();
-    console.log(channel.messages);
+    await channel.populate({ path: 'messages' }).execPopulate();
   } catch (e) {
     return res.send({ ok: false, err: e.message });
   }
 
-  return res.send({ ok: true, channel });
+  return res.send({ ok: true, messages: channel.messages });
+});
+
+router.post('/api/channel/:id/message', auth, channelAuth, async (req, res) => {
+  const { channel, user } = req;
+  let message = null;
+  try {
+    message = new Message({
+      senderId: user.id,
+      channelId: channel.id,
+      content: req.body.message,
+    });
+    await message.save();
+  } catch (e) {
+    return res.send({ ok: false, err: e.message });
+  }
+
+  return res.send({ ok: true, message });
 });
 
 export default router;
