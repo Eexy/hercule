31690cc691e98a45c0c2857e9e7eda9ccbae39a3
unknown <eexdhe@gmail.com>
2021-05-17 15:35:55 +0200

    redo commit

---
+++ apollon/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+import './App.css';
+import Auth from './screens/auth';
+
+function App(): ReactElement {
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
---
+++ apollon/src/screens/auth.tsx
@@ -0,0 +1,5 @@
+import React, { ReactElement } from 'react';
+import AuthForm from './components/auth-form';
+
+const Auth: React.FC = (): ReactElement => <AuthForm />;
+export default Auth;
