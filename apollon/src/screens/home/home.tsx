f68e8c210b56eec045fa1f20e182daba0cd423b4
unknown <eexdhe@gmail.com>
2021-05-21 20:45:10 +0200

    create new project btn

--- apollon/src/screens/home/home.tsx
+++ apollon/src/screens/home/home.tsx
@@ -1,10 +1,33 @@
+import axios from 'axios';
 import React, { ReactElement, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import { UserContext } from '../../context/user-context';
 
 const Home: React.FC = (): ReactElement => {
   const {user} = useContext(UserContext);
-  
-  return <div className="home" />;
+
+  if(user.token === ""){
+    return <Redirect to='/login' />
+  }
+
+  const handleClick = async () => {
+    try{
+      const res = await axios.post('/api/project/new', {
+        name: 'test project',
+      }, {
+        headers: {
+          Authorization: `Bearer ${user.token}`
+        }
+      })
+      console.log(res);
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  return <div className="home">
+    <button type="button" onClick={handleClick}>Create new project</button>
+  </div>;
 };
 
 export default Home;
--- apollon/src/setupProxy.js
+++ apollon/src/setupProxy.js
@@ -1,12 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const createProxyMiddleware = require('http-proxy-middleware');
 
-module.exports = function(app){
-  app.use(createProxyMiddleware('/api',{
-    target: 'http://localhost:4000',
-    changeOrigin: true,
-  }));
-  app.use(createProxyMiddleware('/user', {
-    target: 'https://api.github.com',
-    changeOrigin: true
-  }));  
-}
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    createProxyMiddleware('/user', {
+      target: 'https://api.github.com',
+      changeOrigin: true,
+    })
+  );
+};
