b366ce948a82412d3bc99d4ac30b8e6d99fb7c93
unknown <eexdhe@gmail.com>
2021-05-21 17:58:55 +0200

    clean user's auth route on api

--- atlas/src/routers/user.ts
+++ atlas/src/routers/user.ts
@@ -5,13 +5,13 @@ import getUser from '../utils/get-user';
 const router: Router = express.Router();
 
 router.post('/api/auth', async (req, res) => {
-  const {code} = req.body;
-  try{
+  const { code } = req.body;
+  try {
     const token = await getAuthToken(code);
     const user = await getUser(token);
     return res.send({ ok: true, token, user });
-  }catch(e){
-    return res.send({ok: false, err: e.message});
+  } catch (e) {
+    return res.send({ ok: false, err: e.message });
   }
 });
 
